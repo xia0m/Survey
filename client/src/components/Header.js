f9a3961dd7dc3a94bea2209863539709492975ca
xia0m <xia0m@Zemins-MacBook-Pro.local>
2018-10-17 15:27:49 -0700

    create first action

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
+
 class Header extends Component {
     render(){
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div class="nav-wrapper">
+                <a className="brand-logo">Survey</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><a href="sass.html">Login With Google</a></li>
+                    
+                </ul>
+                </div>
+            </nav>
         )
     }
 }
