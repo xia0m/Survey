b7a5a32c14d362f31d2b2eaaf817f6c6a6e0c9ac
xia0m <xia0m@Zemins-MacBook-Pro.local>
2018-10-17 15:48:37 -0700

    refactor to use async/await in action creator

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
     render(){
         return (
             <nav>
-                <div class="nav-wrapper">
+                <div className="nav-wrapper">
                 <a className="brand-logo">Survey</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><a href="sass.html">Login With Google</a></li>
