47a152d7dbf58c7d2c1a0cb02875046f61cb5ede
xia0m <xia0m@Zemins-MacBook-Pro.local>
2018-10-18 13:26:06 -0700

    added payment button

---
+++ client/src/components/Payments.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+class Payments extends Component {
+    render(){
+        
+        return(
+            <StripeCheckout 
+                name='Survey'
+                description='$5 for 5'
+                amount={500}
+                token={token=>console.log(token)}
+                stripeKey={process.env.REACT_APP_STRIPE_KEY}
+
+            >
+                <button className='btn'>
+                  Add Credits
+                </button>
+            </StripeCheckout>
+        )
+    }
+}
+
+export default Payments;
+
+
