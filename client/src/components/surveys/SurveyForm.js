f7e89c4bb8f0fc11e62cf368bcd86dc8b779c0f8
xia0m <xia0m@Zemins-MacBook-Pro.local>
2018-10-20 13:13:47 -0700

    added survey form file

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class SurveyForm extends Component {
+  render() {
+    return (
+      <div>
+        Survey Form!
+      </div>
+    )
+  }
+}
+
+export default SurveyForm;
