d9eded5d3ceb5000decef95a00e301da6a348b43
xia0m <xia0m@Zemins-MacBook-Pro.local>
2018-10-20 16:33:13 -0700

    added survey review page

--- client/src/components/surveys/SurveyReview.js
+++ client/src/components/surveys/SurveyReview.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const SurveyReview = () => {
+const SurveyReview = ({onCancle}) => {
   return (
     <div>
-      
+        <h5>Please confirm your entries</h5>
+        <button className="yellow darken-3 btn-flat"
+            onClick={onCancle} >
+            Back
+        </button>
     </div>
   );
 };
