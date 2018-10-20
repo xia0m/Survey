f35d7935b16c98f4226c7255dd4f825897f33aaa
xia0m <xia0m@Zemins-MacBook-Pro.local>
2018-10-20 14:02:18 -0700

    create survey component and survye skelton

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default ({input,label}) => {
+    
+  return (
+    <div>
+      <label>{label}</label>
+      <input {...input} />
+    </div>
+  );
+};
+
+
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
+import {Field, reduxForm} from 'redux-form';
+import SurveyField from './SurveyField';
+import _ from 'lodash';
 
+
+const FORM_FIELD = [
+	{label:"Title", name:'title'},
+	{label:"Subject", name:'subject'},
+	{label:"Body", name:'body'},
+	{label:"Recipients List", name:'emails'},
+];
 
 class SurveyForm extends Component {
+
+	renderFields(){
+		return _.map(FORM_FIELD,({label,name})=>{
+			return <Field key={name} component={SurveyField} type="text" label = {label} name={name} />
+		
+		});
+	}
   render() {
     return (
-      <div>
-        Survey Form!
-      </div>
+      <form onSubmit={this.props.handleSubmit(values=>console.log(values))}>
+				{this.renderFields()}
+				<button type="submit">Submit</button>
+      </form>
     )
   }
 }
 
-export default SurveyForm;
+export default reduxForm({
+    form:'surveyForm'
+})(SurveyForm);
