8f222f5b3d8e87d5af702daedf94f34e43e018a9
xia0m <xia0m@Zemins-MacBook-Pro.local>
2018-10-21 16:43:45 -0700

    fix date displaying bug

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -4,10 +4,35 @@ import {fetchSurveys} from '../../actions';
 
 
 class SurveyList extends Component {
+	
+	componentDidMount(){
+		this.props.fetchSurveys();
+	}
+
+	renderSurveys(){
+		return this.props.surveys.map(survey=>{
+			return (
+				<div className="card darken-1" key={survey._id}>
+					<div className="card-content">
+						<span className="card-title">{survey.title}</span>
+						<p>{survey.body}</p>
+						<p className="right">
+							Sent on: {new Date(survey.dateSent).toLocaleDateString()}
+						</p>
+					</div>
+					<div className="card-action">
+						<a href="#">Yes: {survey.yes}</a>
+						<a href="#">No: {survey.no}</a>
+					</div>
+				</div>
+			)
+		})
+	}
+    
   render() {
     return (
       <div>
-        SurveyList
+        {this.renderSurveys()}
       </div>
     )
   };
