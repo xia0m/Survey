0a922f7fb78b4c0c3617b233568ed0ef1850cd44
xia0m <xia0m@Zemins-MacBook-Pro.local>
2019-01-31 16:04:43 -0800

    improved survey email template

--- services/emailTemplates/surveyTemplates.js
+++ services/emailTemplates/surveyTemplates.js
@@ -1,20 +1,113 @@
-const keys = require('../../config/keys');
+const keys = require('../../config/keys').keys
 
-module.exports = (survey)=>{
-    return `
-        <html>
-            <body>
-                <div style="text-align:center">
-                    <h3>We'd like your input!</h3>
-                    <p>Please answer the following question:</p>
-                    <p>${survey.body}</p>
-                    <div>
-                        <a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">Yes</a>
-                    </div>
-                    <div>
-                        <a href="${keys.redirectDomain}/api/surveys/${survey.id}/no">No</a>
-                    </div>
-                </div>
-            </body>
-        </html>`
+module.exports = (survey) => {
+  return `
+  <html>
+  	<head>
+  		<style type="text/css">
+  			@import url('https://fonts.googleapis.com/css?family=Roboto:100,400');
+        html, body, div, span, applet, object, iframe,
+        h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+        a, abbr, acronym, address, big, cite, code,
+        del, dfn, em, img, ins, kbd, q, s, samp,
+        small, strike, strong, sub, sup, tt, var,
+        b, u, i, center,
+        dl, dt, dd, ol, ul, li,
+        fieldset, form, label, legend,
+        table, caption, tbody, tfoot, thead, tr, th, td,
+        article, aside, canvas, details, embed,
+        figure, figcaption, footer, header, hgroup,
+        menu, nav, output, ruby, section, summary,
+        time, mark, audio, video {
+        	margin: 0;
+        	padding: 0;
+        	border: 0;
+        	font-size: 100%;
+        	font: inherit;
+        	vertical-align: baseline;
+        }
+        /* HTML5 display-role reset for older browsers */
+        article, aside, details, figcaption, figure,
+        footer, header, hgroup, menu, nav, section {
+        	display: block;
+        }
+        body {
+        	line-height: 1;
+        }
+        ol, ul {
+        	list-style: none;
+        }
+        blockquote, q {
+        	quotes: none;
+        }
+        blockquote:before, blockquote:after,
+        q:before, q:after {
+        	content: '';
+        	content: none;
+        }
+        table {
+        	border-collapse: collapse;
+        	border-spacing: 0;
+        }
+        a {
+        	text-decoration: none;
+        }
+  			body {
+  		     font-family: 'Roboto', sans-serif;
+  				 font-size: 14px;
+  		   }
+  			h1 {
+  			  font-size: 3rem;
+  			  font-weight: 100;
+  				background-color:#7e57c1;
+  				color: white;
+  				padding: 5rem;
+  			}
+  			h2 {
+          color: black;
+  				margin:1rem 0;
+  				font-size: 2rem;
+  				font-weight: bold;
+  			}
+  			h3 {
+  				margin:2rem 0;
+  				font-size: 1rem;
+  				font-weight: bold;
+  			}
+  			footer {
+  				margin-top: 4rem;
+  				background-color: #5E34B1;
+  				color: white;
+  				padding: 5rem;
+  			}
+  			span {
+  				padding: 0.5rem;
+  			}
+  			p {
+          color: black;
+  				padding: 0.5rem;
+  			}
+  			a {
+  				padding: 1rem 2rem;
+  				margin: 1rem;
+  			}
+  		</style>
+  	</head>
+  	<body>
+  		<div style="text-align: center">
+  			<h1>Ez Survey</h1>
+  			<h2>We'd like your inputs!</h2>
+  			<p>Please, help us answering the folowing question:</p>
+  			<h3>${survey.body}</h3>
+  			<div>
+                  <a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">Yes</a>
+                  <a href="${keys.redirectDomain}/api/surveys/${survey.id}/no">No</a>
+        </div>
+        <footer>
+          <span>Ez Survey &copy; 2018</span>
+        </footer>
+  		</div>
+  	</body>
+  </html>
+    `
 }
