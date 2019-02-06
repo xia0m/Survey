3b7d582d22c7bcd66d34692ff248e28a16c035be
xia0m <xia0m@Zemins-MacBook-Pro.local>
2019-02-06 12:25:12 -0800

    improved feature content implementation

--- client/src/components/Feature/FeatureContent.js
+++ client/src/components/Feature/FeatureContent.js
@@ -1,29 +1,45 @@
-import React from 'react';
+import React,{Component}from 'react';
 
-const FeatureContent = ()=>(
-  <div className="row">
-    <div className="col s12 m4">
-      <h5>
-        <i className="fa fa-user"></i> Unlimited Users
-      </h5>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis deserunt quo ipsum beatae rerum consequuntur maiores
-        voluptates adipisci quaerat sint.</p>
-    </div>
-    <div className="col s12 m4">
-      <h5>
-        <i className="fa fa-envelope"></i> Easy Survey Creation
-      </h5>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis deserunt quo ipsum beatae rerum consequuntur maiores
-        voluptates adipisci quaerat sint.</p>
-    </div>
-    <div className="col s12 m4">
-      <h5>
-        <i className="fa fa-credit-card"></i> Easy Payment
-      </h5>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis deserunt quo ipsum beatae rerum consequuntur maiores
-        voluptates adipisci quaerat sint.</p>
-    </div>
-  </div>
-)
+const newObj = [
+  {
+    name:"Unlimited Users",
+    content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis deserunt quo ipsum beatae rerum consequuntur maiores voluptates adipisci quaerat sint.",
+    classKeyWords:"fa fa-user",
+    id:1
+  },{
+    name:"Easy Survey Creation",
+    content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis deserunt quo ipsum beatae rerum consequuntur maiores voluptates adipisci quaerat sint.",
+    classKeyWords:"fa fa-envelope",
+    id:2
+  },{
+    name:"Easy Payment",
+    content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis deserunt quo ipsum beatae rerum consequuntur maiores voluptates adipisci quaerat sint.",
+    classKeyWords:"fa fa-credit-card",
+    id:3
+  }
+]
+
+class FeatureContent extends Component {
+
+  renderContent() {
+    
+    return newObj.map(entry=>
+      <div className="col s12 m4" id={entry.id}>
+        <h5>
+          <i className={entry.classKeyWords}></i> {entry.name}
+          <p>{entry.content}</p>
+        </h5>
+      </div>
+    )
+  }
+  render(){
+    return(
+      <div className="row">
+      {this.renderContent()}
+      </div>
+    )
+  }
+
+}
 
 export default FeatureContent;
