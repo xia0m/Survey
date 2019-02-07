4ad92eab88e1d92cfee4b6e459e4a0a3f24a509b
xia0m <xia0m@Zemins-MacBook-Pro.local>
2019-02-06 17:01:45 -0800

    implement google login style

--- client/src/components/LoginModal.js
+++ client/src/components/LoginModal.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import '../../node_modules/materialize-social/materialize-social.css';
 
 const LoginModal = ()=>(
   <div id="login-modal" className="modal">
-		Hello!!					
+		<div className="modal-content center">
+      <h4>Account Login</h4>
+      <p>Login to access your account dashboard</p>
+      <div>
+        <a className="waves-effect waves-light btn social google"
+          href="/auth/google">
+          <i class="fa fa-google"></i> Sign in with google
+        </a> 
+      </div>
+    </div>	
 	</div>
 )
 
