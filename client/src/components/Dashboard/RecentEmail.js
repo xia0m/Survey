2ae0574a6849155aece1d8ecce31ea2611b1d195
xia0m <xia0m@Zemins-MacBook-Pro.local>
2019-02-07 17:39:35 -0800

    added recent email skeleton

---
+++ client/src/components/Dashboard/RecentEmail.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RecentEmail = ()=>(
+  <section className="section">
+
+  </section>
+)
+
+export default RecentEmail
--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Navbar from './Navbar';
 import StatSection from './StatSection';
+import RecentEmail from './RecentEmail';
 
 class Dashboard extends Component{
   render(){
@@ -9,6 +10,7 @@ class Dashboard extends Component{
       <div className="grey lighten-4">
         <Navbar />
         <StatSection />
+
       </div>
     )
   }
