271a32dea4a5f9a9d291e2d952d93df31313777f
xia0m <xia0m@Zemins-MacBook-Pro.local>
2019-02-07 16:50:36 -0800

    added Navbar

--- client/src/components/Dashboard/Navbar.js
+++ client/src/components/Dashboard/Navbar.js
@@ -1,12 +1,22 @@
 import React,{Component}from 'react';
 
+import NavbarItem from './NavbarItem'
+
 
 class Navbar extends Component{
   render(){
     return(
-      <div>
-        Navbar!!
-      </div>
+      <nav className="purple lighten-1">
+        <div className="container">
+          <div className="nav-wrapper">
+            <a href="/surveys" className="brand-logo">Ez Survey</a>
+
+            <ul>
+              <NavbarItem />
+            </ul>
+          </div>
+        </div>
+      </nav>
     )
   }
   
---
+++ client/src/components/Dashboard/NavbarItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const NavbarItem = ()=>(
+  <ul className="right">
+    <li class="active">
+      <a href="#">Dashboard</a>
+    </li>
+    <li>
+      <a href="#">Emails</a>
+    </li>
+    <li>
+      <a href="#">Templates</a>
+    </li>
+    <li>
+      <a href="#">Users</a>
+    </li>
+  </ul>
+)
+
+export default NavbarItem;
