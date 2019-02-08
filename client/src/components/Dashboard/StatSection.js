0dd15de3c1ff765191479421b781b1285dc3b6b8
xia0m <xia0m@Zemins-MacBook-Pro.local>
2019-02-07 16:54:05 -0800

    added stat section skeleton

---
+++ client/src/components/Dashboard/StatSection.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const StatSection = ()=>(
+  <div>
+    StatSection
+  </div>
+)
+
+export default StatSection;
--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import Navbar from './Navbar';
+import StatSection from './StatSection';
 
 class Dashboard extends Component{
   render(){
     return(
       <div className="grey lighten-4">
         <Navbar />
+        <StatSection />
       </div>
     )
   }
