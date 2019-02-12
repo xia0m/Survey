a1be6c54546e1dc771a599d5d3cf0ae4c90dd0c5
xia0m <alexma325@gmail.com>
2019-02-12 13:29:28 -0800

    changed class to className in template.js

--- client/src/containers/Templates/index.js
+++ client/src/containers/Templates/index.js
@@ -12,16 +12,16 @@ class Templates extends Component{
       <div className='container'>
         <div className='row'>
           <div className='col s12 m6 l6'>
-            <div class='card'>
-              <div class='card-image waves-effect waves-block waves-light'>
-                <img class='activator' src={templateImage} alt='An email template' />
+            <div className='card'>
+              <div className='card-image waves-effect waves-block waves-light'>
+                <img className='activator' src={templateImage} alt='An email template' />
               </div>
-              <div class='card-content'>
-                <span class='card-title activator grey-text text-darken-4'>Basic Template<i class='material-icons right'>more_vert</i></span>
+              <div className='card-content'>
+                <span className='card-title activator grey-text text-darken-4'>Basic Template<i className='material-icons right'>more_vert</i></span>
                 <p><a href='#'>Preview</a></p>
               </div>
-              <div class='card-reveal'>
-                <span class='card-title grey-text text-darken-4'>Basic Template<i class='material-icons right'>close</i></span>
+              <div className='card-reveal'>
+                <span className='card-title grey-text text-darken-4'>Basic Template<i className='material-icons right'>close</i></span>
                 <p>The Email your customer will see.</p>
               </div>
             </div>
