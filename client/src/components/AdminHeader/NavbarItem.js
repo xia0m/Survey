3014a03214a0be99f5ce082010e07ac94d59b96b
xia0m <alexma325@gmail.com>
2019-02-12 12:37:19 -0800

    added dropdown button

--- client/src/components/AdminHeader/NavbarItem.js
+++ client/src/components/AdminHeader/NavbarItem.js
@@ -2,22 +2,6 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 
 const NavbarItem = ({location})=>(
-  // <ul className='right'>
-  //   <li class='active'>
-  //     <a href='/dashboard'>Dashboard</a>
-  //   </li>
-  //   <li>
-  //     <a href='/surveys'>Surveys</a>
-  //   </li>
-  //   <li>
-  //     <a href='/templates'>Templates</a>
-  //   </li>
-  //   <li>
-  //     <a className='dropdown-trigger' data-target='user-dropdown' href='/users'>
-  //       User<i className='material-icons right'>arrow_drop_down</i>
-  //     </a>
-  //   </li>
-  // </ul>
   <ul className='right'>
     <li>
       <NavLink 
@@ -41,6 +25,11 @@ const NavbarItem = ({location})=>(
         to='/templates'>
         Templates
       </NavLink>
+    </li>
+    <li>
+      <a className='dropdown-trigger' data-target='user-dropdown' href='/users'>
+       User<i className='material-icons right'>arrow_drop_down</i>
+      </a>
     </li>
   </ul>
 )
