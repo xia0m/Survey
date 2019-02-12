1b84f0f2fd2e1f90967702297911e146faa59594
xia0m <alexma325@gmail.com>
2019-02-12 11:23:16 -0800

    import cleanup

--- client/src/containers/Emails/index.js
+++ client/src/containers/Emails/index.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-import Navbar from '../../components/AdminHeader/Navbar';
-import Footer from '../../components/AdminFooter/Footer';
 import SurveyList from '../../components/surveys/SurveyList';
 
 class Emails extends Component{
