b917d3fa74f4a9fb127723beba732c9a06de23a4
xia0m <alexma325@gmail.com>
2019-02-13 16:35:03 -0800

    changed users to templates on dashboard page

--- client/src/components/StatCard/StatSection.js
+++ client/src/components/StatCard/StatSection.js
@@ -1,37 +1,43 @@
-import React from 'react';
+import React,{Component} from 'react';
 
 import StatCard from './StatCard';
 
-const StatSection = ()=>(
-  <section className="section center">
-    <div className="row">
-      <StatCard 
-        cardStyle={'red red-text'}
-        iconName={'feedback'}
-        cardName={'Feedbacks'}
-        cardNumber={2}
-      />
-      <StatCard 
-        cardStyle={'blue blue-text'}
-        iconName={'email'}
-        cardName={'Email Sent'}
-        cardNumber={5}
-      />
-      <StatCard 
-        cardStyle={'teal teal-text'}
-        iconName={'account_balance'}
-        cardName={'Credits Avaliable'}
-        cardNumber={15}
-      />
-      <StatCard 
-        cardStyle={'green green-text'}
-        iconName={'supervisor_account'}
-        cardName={'Users'}
-        cardNumber={2}
-      />
-      
-    </div>
-  </section>
-)
+class StatSection extends Component{
+
+  render(){
+    return(
+      <section className="section center">
+        <div className="row">
+          <StatCard 
+            cardStyle={'red red-text'}
+            iconName={'feedback'}
+            cardName={'Feedbacks'}
+            cardNumber={2}
+          />
+          <StatCard 
+            cardStyle={'blue blue-text'}
+            iconName={'email'}
+            cardName={'Email Sent'}
+            cardNumber={5}
+          />
+          <StatCard 
+            cardStyle={'teal teal-text'}
+            iconName={'account_balance'}
+            cardName={'Credits Avaliable'}
+            cardNumber={15}
+          />
+          <StatCard 
+            cardStyle={'green green-text'}
+            iconName={'folder_open'}
+            cardName={'Templates'}
+            cardNumber={1}
+          />
+        </div>
+     </section>
+    )
+  }
+}
+  
+
 
 export default StatSection;
